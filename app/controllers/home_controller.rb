06a1d2122bf6d885419e224a9ba6c5ed79483b3e
tonnnnngirl <huomentachisato@gmail.com>
2018-10-31 04:36:31 +0900

    colored

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def top
   end
+
+  def search
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/'=>'home#top'
+  get '/search' => 'home#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
